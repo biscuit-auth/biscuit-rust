34d014cdb0c37f89890db238be6008a128967c3b
Geoffroy Couprie <geo.couprie@gmail.com>
2019-12-18 11:35:02 +0100

    benchmark verification

--- benches/token.rs
+++ benches/token.rs
@@ -234,7 +234,7 @@ fn check_signature_2(b: &mut Bencher) {
 
     let token = builder.build().unwrap();
     let base_data = token.to_vec().unwrap();
-    
+
     let mut block_builder = token.create_block();
     block_builder.check_resource("file1");
     block_builder.check_operation("read");
@@ -345,6 +345,81 @@ fn caveats_block_2(b: &mut Bencher) {
   b.bytes = data.len() as u64;
   b.iter(|| {
     let mut verifier = token.verify(root.public()).unwrap();
+    verifier.add_resource("file1");
+    verifier.add_operation("read");
+    verifier.verify().unwrap();
+  });
+}
+
+#[bench]
+fn caveats_block_create_verifier2(b: &mut Bencher) {
+  let mut rng: OsRng = OsRng;
+  let root = KeyPair::new(&mut rng);
+  let keypair2 = KeyPair::new(&mut rng);
+
+  let data = {
+    let mut builder = Biscuit::builder(&mut rng, &root);
+    builder.add_authority_fact(fact("right", &[s("authority"), string("file1"), s("read")],));
+    builder.add_authority_fact(fact("right", &[s("authority"), string("file2"), s("read")],));
+    builder.add_authority_fact(fact("right", &[s("authority"), string("file1"), s("write")],));
+
+    let token = builder.build().unwrap();
+    let base_data = token.to_vec().unwrap();
+
+    let mut block_builder = token.create_block();
+    block_builder.check_resource("file1");
+    block_builder.check_operation("read");
+
+    let token2 = token.append(&mut rng, &keypair2, block_builder.build()).unwrap();
+    token2.to_vec().unwrap()
+  };
+
+  let token = Biscuit::from(&data).unwrap();
+  let mut verifier = token.verify(root.public()).unwrap();
+  verifier.add_resource("file1");
+  verifier.add_operation("read");
+  verifier.verify().unwrap();
+
+  let token = Biscuit::from(&data).unwrap();
+  b.bytes = data.len() as u64;
+  b.iter(|| {
+    let mut verifier = token.verify(root.public()).unwrap();
+  });
+}
+
+#[bench]
+fn caveats_block_verify_only2(b: &mut Bencher) {
+  let mut rng: OsRng = OsRng;
+  let root = KeyPair::new(&mut rng);
+  let keypair2 = KeyPair::new(&mut rng);
+
+  let data = {
+    let mut builder = Biscuit::builder(&mut rng, &root);
+    builder.add_authority_fact(fact("right", &[s("authority"), string("file1"), s("read")],));
+    builder.add_authority_fact(fact("right", &[s("authority"), string("file2"), s("read")],));
+    builder.add_authority_fact(fact("right", &[s("authority"), string("file1"), s("write")],));
+
+    let token = builder.build().unwrap();
+    let base_data = token.to_vec().unwrap();
+
+    let mut block_builder = token.create_block();
+    block_builder.check_resource("file1");
+    block_builder.check_operation("read");
+
+    let token2 = token.append(&mut rng, &keypair2, block_builder.build()).unwrap();
+    token2.to_vec().unwrap()
+  };
+
+  let token = Biscuit::from(&data).unwrap();
+  let mut verifier = token.verify(root.public()).unwrap();
+  verifier.add_resource("file1");
+  verifier.add_operation("read");
+  verifier.verify().unwrap();
+
+  let token = Biscuit::from(&data).unwrap();
+  let mut verifier = token.verify(root.public()).unwrap();
+  b.iter(|| {
+    verifier.reset();
     verifier.add_resource("file1");
     verifier.add_operation("read");
     verifier.verify().unwrap();
