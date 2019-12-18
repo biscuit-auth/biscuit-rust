579602b0d3b6c9576b81174bd0c0e8c9c2193378
Geoffroy Couprie <geo.couprie@gmail.com>
2019-12-18 16:58:16 +0100

    use a separate world for the current test

--- src/token/verifier.rs
+++ src/token/verifier.rs
@@ -9,49 +9,68 @@ use std::{collections::HashMap, convert::TryInto, time::SystemTime};
 
 pub struct Verifier<'a> {
     token: &'a Biscuit,
+    base_world: datalog::World,
     world: datalog::World,
-    facts: Vec<Fact>,
-    rules: Vec<Rule>,
     caveats: Vec<Rule>,
     queries: HashMap<String, Rule>,
 }
 
 impl<'a> Verifier<'a> {
     pub(crate) fn new(token: &'a Biscuit) -> Result<Self, error::Logic> {
-        let world = token.generate_world(&token.symbols)?;
+        let base_world = token.generate_world(&token.symbols)?;
+        let world = base_world.clone();
         Ok(Verifier {
             token,
+            base_world,
             world,
-            facts: vec![],
-            rules: vec![],
             caveats: vec![],
             queries: HashMap::new(),
         })
     }
 
     pub fn reset(&mut self) {
-        self.facts.clear();
-        self.rules.clear();
         self.caveats.clear();
         self.queries.clear();
+        self.world = self.base_world.clone();
     }
 
     pub fn add_fact<F: TryInto<Fact>>(&mut self, fact: F) -> Result<(), error::Token> {
         let fact = fact.try_into().map_err(|_| error::Token::ParseError)?;
-        self.facts.push(fact);
+        let mut symbols = self.token.symbols.clone();
+        self.world.facts.insert(fact.convert(&mut symbols));
         Ok(())
     }
 
     pub fn add_rule<R: TryInto<Rule>>(&mut self, rule: R) -> Result<(), error::Token> {
         let rule = rule.try_into().map_err(|_| error::Token::ParseError)?;
-        self.rules.push(rule);
+        let mut symbols = self.token.symbols.clone();
+        self.world.rules.push(rule.convert(&mut symbols));
         Ok(())
     }
 
-    pub fn add_query<S: Into<String>, R: TryInto<Rule>>(&mut self, name: S, rule: R) -> Result<(), error::Token> {
+    pub fn add_query<S: Into<String>, R: TryInto<Rule>>(
+        &mut self,
+        name: S,
+        rule: R,
+    ) -> Result<(), error::Token> {
         let rule = rule.try_into().map_err(|_| error::Token::ParseError)?;
         self.queries.insert(name.into(), rule);
         Ok(())
+    }
+
+    pub fn query<R: TryInto<Rule>>(
+        &mut self,
+        rule: R,
+    ) -> Result<Vec<Fact>, error::Token> {
+        let rule = rule.try_into().map_err(|_| error::Token::ParseError)?;
+        let mut symbols = self.token.symbols.clone();
+        self.world.run();
+        let mut res = self.world.query_rule(rule.convert(&mut symbols));
+
+        Ok(res
+           .drain(..)
+           .map(|f| Fact::convert_from(&f, &symbols))
+           .collect())
     }
 
     /// verifier caveats
@@ -62,20 +81,21 @@ impl<'a> Verifier<'a> {
     }
 
     pub fn add_resource(&mut self, resource: &str) {
-        self.facts
-            .push(fact("resource", &[s("ambient"), string(resource)]));
+        let fact = fact("resource", &[s("ambient"), string(resource)]);
+        let mut symbols = self.token.symbols.clone();
+        self.world.facts.insert(fact.convert(&mut symbols));
     }
 
     pub fn add_operation(&mut self, operation: &str) {
-        self.facts
-            .push(fact("operation", &[s("ambient"), s(operation)]));
+        let fact = fact("operation", &[s("ambient"), s(operation)]);
+        let mut symbols = self.token.symbols.clone();
+        self.world.facts.insert(fact.convert(&mut symbols));
     }
 
     pub fn set_time(&mut self) {
-        self.facts.retain(|f| f.0.name != "time");
-
-        self.facts
-            .push(fact("time", &[s("ambient"), date(&SystemTime::now())]));
+        let fact = fact("time", &[s("ambient"), date(&SystemTime::now())]);
+        let mut symbols = self.token.symbols.clone();
+        self.world.facts.insert(fact.convert(&mut symbols));
     }
 
     pub fn revocation_check(&mut self, ids: &[i64]) {
@@ -91,7 +111,7 @@ impl<'a> Verifier<'a> {
         let _ = self.add_caveat(caveat);
     }
 
-    pub fn verify(&self) -> Result<HashMap<String, Vec<Fact>>, error::Token> {
+    pub fn verify(&mut self) -> Result<HashMap<String, Vec<Fact>>, error::Token> {
         let mut symbols = self.token.symbols.clone();
 
         //FIXME: should check for the presence of any other symbol in the token
@@ -99,22 +119,14 @@ impl<'a> Verifier<'a> {
             return Err(error::Token::MissingSymbols);
         }
 
-        let mut world = self.world.clone();
+        self.world.run();
 
         let mut queries = HashMap::new();
-
-        for fact in self.facts.iter() {
-            world.facts.insert(fact.convert(&mut symbols));
-        }
-
-        for rule in self.rules.iter() {
-            world.rules.push(rule.convert(&mut symbols));
-        }
 
         let mut errors = vec![];
         for (i, caveat) in self.caveats.iter().enumerate() {
             let c = caveat.convert(&mut symbols);
-            let res = world.query_rule(c.clone());
+            let res = self.world.query_rule(c.clone());
             if res.is_empty() {
                 errors.push(error::FailedCaveat::Verifier(error::FailedVerifierCaveat {
                     block_id: 0,
@@ -127,7 +139,7 @@ impl<'a> Verifier<'a> {
         for (i, block_caveats) in self.token.caveats().iter().enumerate() {
             for (j, caveat) in block_caveats.iter().enumerate() {
                 println!("adding caveat to verifier: ({},{}) {:?}", i, j, caveat);
-                let res = world.query_rule(caveat.clone());
+                let res = self.world.query_rule(caveat.clone());
                 if res.is_empty() {
                     errors.push(error::FailedCaveat::Block(error::FailedBlockCaveat {
                         block_id: i as u32,
@@ -150,7 +162,7 @@ impl<'a> Verifier<'a> {
 
         let mut query_results = HashMap::new();
         for (name, rule) in queries.iter() {
-            let res = world.query_rule(rule.clone());
+            let res = self.world.query_rule(rule.clone());
             query_results.insert(name.clone(), res);
         }
 
