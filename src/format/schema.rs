44e143929fea934ba5719d5e9dad609c496bc519
Geoffroy Couprie <contact@geoffroycouprie.com>
2021-01-04 10:11:34 +0100

    update prost

--- src/format/schema.rs
+++ src/format/schema.rs
@@ -1,44 +1,44 @@
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Biscuit {
-    #[prost(bytes, required, tag="1")]
-    pub authority: std::vec::Vec<u8>,
-    #[prost(bytes, repeated, tag="2")]
-    pub blocks: ::std::vec::Vec<std::vec::Vec<u8>>,
-    #[prost(bytes, repeated, tag="3")]
-    pub keys: ::std::vec::Vec<std::vec::Vec<u8>>,
+    #[prost(bytes="vec", required, tag="1")]
+    pub authority: ::prost::alloc::vec::Vec<u8>,
+    #[prost(bytes="vec", repeated, tag="2")]
+    pub blocks: ::prost::alloc::vec::Vec<::prost::alloc::vec::Vec<u8>>,
+    #[prost(bytes="vec", repeated, tag="3")]
+    pub keys: ::prost::alloc::vec::Vec<::prost::alloc::vec::Vec<u8>>,
     #[prost(message, required, tag="4")]
     pub signature: Signature,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct SealedBiscuit {
-    #[prost(bytes, required, tag="1")]
-    pub authority: std::vec::Vec<u8>,
-    #[prost(bytes, repeated, tag="2")]
-    pub blocks: ::std::vec::Vec<std::vec::Vec<u8>>,
-    #[prost(bytes, required, tag="3")]
-    pub signature: std::vec::Vec<u8>,
+    #[prost(bytes="vec", required, tag="1")]
+    pub authority: ::prost::alloc::vec::Vec<u8>,
+    #[prost(bytes="vec", repeated, tag="2")]
+    pub blocks: ::prost::alloc::vec::Vec<::prost::alloc::vec::Vec<u8>>,
+    #[prost(bytes="vec", required, tag="3")]
+    pub signature: ::prost::alloc::vec::Vec<u8>,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Signature {
-    #[prost(bytes, repeated, tag="1")]
-    pub parameters: ::std::vec::Vec<std::vec::Vec<u8>>,
-    #[prost(bytes, required, tag="2")]
-    pub z: std::vec::Vec<u8>,
+    #[prost(bytes="vec", repeated, tag="1")]
+    pub parameters: ::prost::alloc::vec::Vec<::prost::alloc::vec::Vec<u8>>,
+    #[prost(bytes="vec", required, tag="2")]
+    pub z: ::prost::alloc::vec::Vec<u8>,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Block {
     #[prost(uint32, required, tag="1")]
     pub index: u32,
     #[prost(string, repeated, tag="2")]
-    pub symbols: ::std::vec::Vec<std::string::String>,
+    pub symbols: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
     #[prost(message, repeated, tag="3")]
-    pub facts: ::std::vec::Vec<Fact>,
+    pub facts: ::prost::alloc::vec::Vec<Fact>,
     #[prost(message, repeated, tag="4")]
-    pub rules: ::std::vec::Vec<Rule>,
+    pub rules: ::prost::alloc::vec::Vec<Rule>,
     #[prost(message, repeated, tag="5")]
-    pub caveats: ::std::vec::Vec<Caveat>,
+    pub caveats: ::prost::alloc::vec::Vec<Caveat>,
     #[prost(string, optional, tag="6")]
-    pub context: ::std::option::Option<std::string::String>,
+    pub context: ::core::option::Option<::prost::alloc::string::String>,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Fact {
@@ -50,39 +50,40 @@ pub struct Rule {
     #[prost(message, required, tag="1")]
     pub head: Predicate,
     #[prost(message, repeated, tag="2")]
-    pub body: ::std::vec::Vec<Predicate>,
+    pub body: ::prost::alloc::vec::Vec<Predicate>,
     #[prost(message, repeated, tag="3")]
-    pub constraints: ::std::vec::Vec<Constraint>,
+    pub constraints: ::prost::alloc::vec::Vec<Constraint>,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Caveat {
     #[prost(message, repeated, tag="1")]
-    pub queries: ::std::vec::Vec<Rule>,
+    pub queries: ::prost::alloc::vec::Vec<Rule>,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Predicate {
     #[prost(uint64, required, tag="1")]
     pub name: u64,
     #[prost(message, repeated, tag="2")]
-    pub ids: ::std::vec::Vec<Id>,
+    pub ids: ::prost::alloc::vec::Vec<Id>,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Id {
     #[prost(enumeration="id::Kind", required, tag="1")]
     pub kind: i32,
     #[prost(uint64, optional, tag="2")]
-    pub symbol: ::std::option::Option<u64>,
+    pub symbol: ::core::option::Option<u64>,
     #[prost(uint32, optional, tag="3")]
-    pub variable: ::std::option::Option<u32>,
+    pub variable: ::core::option::Option<u32>,
     #[prost(int64, optional, tag="4")]
-    pub integer: ::std::option::Option<i64>,
+    pub integer: ::core::option::Option<i64>,
     #[prost(string, optional, tag="5")]
-    pub str: ::std::option::Option<std::string::String>,
+    pub str: ::core::option::Option<::prost::alloc::string::String>,
     #[prost(uint64, optional, tag="6")]
-    pub date: ::std::option::Option<u64>,
-    #[prost(bytes, optional, tag="7")]
-    pub bytes: ::std::option::Option<std::vec::Vec<u8>>,
+    pub date: ::core::option::Option<u64>,
+    #[prost(bytes="vec", optional, tag="7")]
+    pub bytes: ::core::option::Option<::prost::alloc::vec::Vec<u8>>,
 }
+/// Nested message and enum types in `ID`.
 pub mod id {
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
     #[repr(i32)]
@@ -102,16 +103,17 @@ pub struct Constraint {
     #[prost(enumeration="constraint::Kind", required, tag="2")]
     pub kind: i32,
     #[prost(message, optional, tag="3")]
-    pub int: ::std::option::Option<IntConstraint>,
+    pub int: ::core::option::Option<IntConstraint>,
     #[prost(message, optional, tag="4")]
-    pub str: ::std::option::Option<StringConstraint>,
+    pub str: ::core::option::Option<StringConstraint>,
     #[prost(message, optional, tag="5")]
-    pub date: ::std::option::Option<DateConstraint>,
+    pub date: ::core::option::Option<DateConstraint>,
     #[prost(message, optional, tag="6")]
-    pub symbol: ::std::option::Option<SymbolConstraint>,
+    pub symbol: ::core::option::Option<SymbolConstraint>,
     #[prost(message, optional, tag="7")]
-    pub bytes: ::std::option::Option<BytesConstraint>,
+    pub bytes: ::core::option::Option<BytesConstraint>,
 }
+/// Nested message and enum types in `Constraint`.
 pub mod constraint {
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
     #[repr(i32)]
@@ -128,20 +130,21 @@ pub struct IntConstraint {
     #[prost(enumeration="int_constraint::Kind", required, tag="1")]
     pub kind: i32,
     #[prost(int64, optional, tag="2")]
-    pub lower: ::std::option::Option<i64>,
+    pub lower: ::core::option::Option<i64>,
     #[prost(int64, optional, tag="3")]
-    pub larger: ::std::option::Option<i64>,
+    pub larger: ::core::option::Option<i64>,
     #[prost(int64, optional, tag="4")]
-    pub lower_or_equal: ::std::option::Option<i64>,
+    pub lower_or_equal: ::core::option::Option<i64>,
     #[prost(int64, optional, tag="5")]
-    pub larger_or_equal: ::std::option::Option<i64>,
+    pub larger_or_equal: ::core::option::Option<i64>,
     #[prost(int64, optional, tag="6")]
-    pub equal: ::std::option::Option<i64>,
+    pub equal: ::core::option::Option<i64>,
     #[prost(int64, repeated, tag="7")]
-    pub in_set: ::std::vec::Vec<i64>,
+    pub in_set: ::prost::alloc::vec::Vec<i64>,
     #[prost(int64, repeated, tag="8")]
-    pub not_in_set: ::std::vec::Vec<i64>,
+    pub not_in_set: ::prost::alloc::vec::Vec<i64>,
 }
+/// Nested message and enum types in `IntConstraint`.
 pub mod int_constraint {
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
     #[repr(i32)]
@@ -160,18 +163,19 @@ pub struct StringConstraint {
     #[prost(enumeration="string_constraint::Kind", required, tag="1")]
     pub kind: i32,
     #[prost(string, optional, tag="2")]
-    pub prefix: ::std::option::Option<std::string::String>,
+    pub prefix: ::core::option::Option<::prost::alloc::string::String>,
     #[prost(string, optional, tag="3")]
-    pub suffix: ::std::option::Option<std::string::String>,
+    pub suffix: ::core::option::Option<::prost::alloc::string::String>,
     #[prost(string, optional, tag="4")]
-    pub equal: ::std::option::Option<std::string::String>,
+    pub equal: ::core::option::Option<::prost::alloc::string::String>,
     #[prost(string, repeated, tag="5")]
-    pub in_set: ::std::vec::Vec<std::string::String>,
+    pub in_set: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
     #[prost(string, repeated, tag="6")]
-    pub not_in_set: ::std::vec::Vec<std::string::String>,
+    pub not_in_set: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
     #[prost(string, optional, tag="7")]
-    pub regex: ::std::option::Option<std::string::String>,
+    pub regex: ::core::option::Option<::prost::alloc::string::String>,
 }
+/// Nested message and enum types in `StringConstraint`.
 pub mod string_constraint {
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
     #[repr(i32)]
@@ -189,10 +193,11 @@ pub struct DateConstraint {
     #[prost(enumeration="date_constraint::Kind", required, tag="1")]
     pub kind: i32,
     #[prost(uint64, optional, tag="2")]
-    pub before: ::std::option::Option<u64>,
+    pub before: ::core::option::Option<u64>,
     #[prost(uint64, optional, tag="3")]
-    pub after: ::std::option::Option<u64>,
+    pub after: ::core::option::Option<u64>,
 }
+/// Nested message and enum types in `DateConstraint`.
 pub mod date_constraint {
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
     #[repr(i32)]
@@ -206,10 +211,11 @@ pub struct SymbolConstraint {
     #[prost(enumeration="symbol_constraint::Kind", required, tag="1")]
     pub kind: i32,
     #[prost(uint64, repeated, packed="false", tag="2")]
-    pub in_set: ::std::vec::Vec<u64>,
+    pub in_set: ::prost::alloc::vec::Vec<u64>,
     #[prost(uint64, repeated, packed="false", tag="3")]
-    pub not_in_set: ::std::vec::Vec<u64>,
+    pub not_in_set: ::prost::alloc::vec::Vec<u64>,
 }
+/// Nested message and enum types in `SymbolConstraint`.
 pub mod symbol_constraint {
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
     #[repr(i32)]
@@ -222,13 +228,14 @@ pub mod symbol_constraint {
 pub struct BytesConstraint {
     #[prost(enumeration="bytes_constraint::Kind", required, tag="1")]
     pub kind: i32,
-    #[prost(bytes, optional, tag="2")]
-    pub equal: ::std::option::Option<std::vec::Vec<u8>>,
-    #[prost(bytes, repeated, tag="3")]
-    pub in_set: ::std::vec::Vec<std::vec::Vec<u8>>,
-    #[prost(bytes, repeated, tag="4")]
-    pub not_in_set: ::std::vec::Vec<std::vec::Vec<u8>>,
+    #[prost(bytes="vec", optional, tag="2")]
+    pub equal: ::core::option::Option<::prost::alloc::vec::Vec<u8>>,
+    #[prost(bytes="vec", repeated, tag="3")]
+    pub in_set: ::prost::alloc::vec::Vec<::prost::alloc::vec::Vec<u8>>,
+    #[prost(bytes="vec", repeated, tag="4")]
+    pub not_in_set: ::prost::alloc::vec::Vec<::prost::alloc::vec::Vec<u8>>,
 }
+/// Nested message and enum types in `BytesConstraint`.
 pub mod bytes_constraint {
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
     #[repr(i32)]
