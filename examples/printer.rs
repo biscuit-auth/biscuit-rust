dd61460b24194f56218b996899bc1bea4976f705
Geoffroy Couprie <contact@geoffroycouprie.com>
2021-01-05 11:38:35 +0100

    example token printer

---
+++ examples/printer.rs
@@ -0,0 +1,17 @@
+
+fn main() {
+    let mut args = std::env::args();
+    args.next();
+    let target = match args.next() {
+        Some(arg) => arg,
+        None => {
+            println!("missing token path argument");
+            return;
+        }
+    };
+
+    let data = std::fs::read(target).unwrap();
+    let token = biscuit_auth::token::Biscuit::from(&data[..]).unwrap();
+
+    println!("Token content:\n{}", token.print());
+}
