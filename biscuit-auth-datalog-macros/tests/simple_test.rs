4e7e37ae377c69ca8e99f73ae618621817853433
Clément Delafargue <clement@delafargue.name>
2022-05-20 00:28:27 +0200

    Test block builder contents in macro tests

--- biscuit-auth-datalog-macros/tests/simple_test.rs
+++ biscuit-auth-datalog-macros/tests/simple_test.rs
@@ -1,44 +1,31 @@
-extern crate biscuit_auth;
 extern crate biscuit_quote;
-use biscuit_auth::{Biscuit, KeyPair};
 use biscuit_quote::block;
 
 #[test]
 fn it_works() {
-    let root = KeyPair::new();
-    let mut biscuit_builder = Biscuit::builder(&root);
-
-    biscuit_builder
-        .add_authority_fact("right(\"/a/file1.txt\", \"read\")")
-        .unwrap();
-    let biscuit = biscuit_builder.build().unwrap();
-
-    let new = biscuit
-        .append(block!(
-            r#"fact("test", hex:aabbcc, [true], {my_key});
+    let b = block!(
+        r#"fact("test", hex:aabbcc, [true], {my_key});
             rule($0, true) <- fact($0, $1, $2, {my_key});
             check if {my_key}.starts_with("my");
             "#,
-            my_key = "my_value",
-            other_key = false,
-        ))
-        .unwrap();
-    println!("biscuit: {}", new.print());
-    panic!("no");
-}
-
-/*
-#[test]
-fn it_works_with_2_parameters() {
-    let toto = block!(r#"fact("test");"#, my_key = "my_value", my_key2 = 42);
-    dbg!(toto);
-    panic!("no");
+        my_key = "my_value",
+        other_key = false,
+    );
+    assert_eq!(
+        b.to_string(),
+        r#"fact("test", hex:aabbcc, [ true], "my_value");
+rule($0, true) <- fact($0, $1, $2, "my_value");
+check if "my_value".starts_with("my");
+"#,
+    );
 }
 
 #[test]
 fn it_works_trailing_comma() {
-    let toto = block!(r#"fact("test");"#, my_key = "my_value",);
-    dbg!(toto);
-    panic!("no");
+    let b = block!(r#"fact("test");"#, my_key = "my_value",);
+    assert_eq!(
+        b.to_string(),
+        r#"fact("test");
+"#,
+    );
 }
-*/
