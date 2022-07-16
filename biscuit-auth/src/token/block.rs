2812559e31011dfc1a9fe2304b356306f20d3da1
Geoffroy Couprie <contact@geoffroycouprie.com>
2022-07-16 16:51:39 +0200

    move block implementation to its own file

---
+++ biscuit-auth/src/token/block.rs
@@ -0,0 +1,72 @@
+use std::collections::{BTreeSet, HashMap};
+
+use crate::{
+    crypto::PublicKey,
+    datalog::{Check, Fact, Rule, SymbolTable, Term},
+};
+
+use super::{public_keys::PublicKeys, Scope};
+
+/// a block contained in a token
+#[derive(Clone, Debug)]
+pub struct Block {
+    /// list of symbols introduced by this block
+    pub symbols: SymbolTable,
+    /// list of facts provided by this block
+    pub facts: Vec<Fact>,
+    /// list of rules provided by this block
+    pub rules: HashMap<BTreeSet<usize>, Vec<Rule>>,
+    /// checks that the token and ambient data must validate
+    pub checks: Vec<(Check, Vec<BTreeSet<usize>>)>,
+    /// contextual information that can be looked up before the verification
+    /// (as an example, a user id to query rights into a database)
+    pub context: Option<String>,
+    /// format version used to generate this block
+    pub version: u32,
+    /// key used in optional external signature
+    pub external_key: Option<PublicKey>,
+    /// list of public keys referenced by this block
+    pub public_keys: PublicKeys,
+    /// list of scopes defining which blocks are trusted by this block
+    pub scopes: Vec<Scope>,
+}
+
+impl Block {
+    pub fn symbol_add(&mut self, s: &str) -> Term {
+        self.symbols.add(s)
+    }
+
+    pub fn symbol_insert(&mut self, s: &str) -> u64 {
+        self.symbols.insert(s)
+    }
+
+    pub(crate) fn print_source(&self, symbols: &SymbolTable) -> String {
+        let facts: Vec<_> = self.facts.iter().map(|f| symbols.print_fact(f)).collect();
+        let rules: Vec<_> = self
+            .rules
+            .iter()
+            .map(|(origin, rules)| rules.iter().map(|r| symbols.print_rule(r)))
+            .flatten()
+            .collect();
+        let checks: Vec<_> = self
+            .checks
+            .iter()
+            .map(|(check, origins)| symbols.print_check(check))
+            .collect();
+
+        let mut res = facts.join(";\n");
+        if !facts.is_empty() {
+            res.push_str(";\n");
+        }
+        res.push_str(&rules.join(";\n"));
+        if !rules.is_empty() {
+            res.push_str(";\n");
+        }
+        res.push_str(&checks.join(";\n"));
+        if !checks.is_empty() {
+            res.push_str(";\n");
+        }
+
+        res
+    }
+}
--- biscuit-auth/src/token/mod.rs
+++ biscuit-auth/src/token/mod.rs
@@ -1,7 +1,9 @@
 //! main structures to interact with Biscuit tokens
+use std::collections::{BTreeSet, HashMap};
+
 use self::public_keys::PublicKeys;
 use super::crypto::{KeyPair, PublicKey};
-use super::datalog::{Check, Fact, Rule, SymbolTable, Term};
+use super::datalog::SymbolTable;
 use super::error;
 use super::format::SerializedBiscuit;
 use builder::{BiscuitBuilder, BlockBuilder};
@@ -12,9 +14,12 @@ use crate::format::{convert::proto_block_to_token_block, schema};
 use authorizer::Authorizer;
 
 pub mod authorizer;
+pub(crate) mod block;
 pub mod builder;
-mod public_keys;
+pub(crate) mod public_keys;
 pub mod unverified;
+
+pub use block::Block;
 
 /// minimum supported version of the serialization format
 pub const MIN_SCHEMA_VERSION: u32 = 3;
@@ -482,61 +487,6 @@ fn print_block(symbols: &SymbolTable, block: &Block) -> String {
         rules,
         checks,
     )
-}
-
-/// a block contained in a token
-#[derive(Clone, Debug)]
-pub struct Block {
-    /// list of symbols introduced by this block
-    pub symbols: SymbolTable,
-    /// list of facts provided by this block
-    pub facts: Vec<Fact>,
-    /// list of rules provided by this block
-    pub rules: Vec<Rule>,
-    /// checks that the token and ambient data must validate
-    pub checks: Vec<Check>,
-    /// contextual information that can be looked up before the verification
-    /// (as an example, a user id to query rights into a database)
-    pub context: Option<String>,
-    /// format version used to generate this block
-    pub version: u32,
-    /// key used in optional external signature
-    pub external_key: Option<PublicKey>,
-    /// list of public keys referenced by this block
-    pub public_keys: Vec<PublicKey>,
-    /// list of scopes defining which blocks are trusted by this block
-    pub scopes: Vec<Scope>,
-}
-
-impl Block {
-    pub fn symbol_add(&mut self, s: &str) -> Term {
-        self.symbols.add(s)
-    }
-
-    pub fn symbol_insert(&mut self, s: &str) -> u64 {
-        self.symbols.insert(s)
-    }
-
-    fn print_source(&self, symbols: &SymbolTable) -> String {
-        let facts: Vec<_> = self.facts.iter().map(|f| symbols.print_fact(f)).collect();
-        let rules: Vec<_> = self.rules.iter().map(|r| symbols.print_rule(r)).collect();
-        let checks: Vec<_> = self.checks.iter().map(|r| symbols.print_check(r)).collect();
-
-        let mut res = facts.join(";\n");
-        if !facts.is_empty() {
-            res.push_str(";\n");
-        }
-        res.push_str(&rules.join(";\n"));
-        if !rules.is_empty() {
-            res.push_str(";\n");
-        }
-        res.push_str(&checks.join(";\n"));
-        if !checks.is_empty() {
-            res.push_str(";\n");
-        }
-
-        res
-    }
 }
 
 #[derive(Clone, Debug, Hash, PartialEq, Eq)]
