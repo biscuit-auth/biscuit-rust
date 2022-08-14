f16d74d087ddf8853abec30ed5683797e32fe510
Clément Delafargue <clement@delafargue.name>
2022-08-14 11:22:20 +0200

    3rd-party: add test for pubkey substitution in macros

--- biscuit-auth-datalog-macros/tests/simple_test.rs
+++ biscuit-auth-datalog-macros/tests/simple_test.rs
@@ -1,5 +1,6 @@
 extern crate biscuit_auth;
 extern crate biscuit_quote;
+extern crate hex;
 use biscuit_quote::{authorizer, biscuit, block};
 
 #[test]
@@ -63,19 +64,25 @@ fn authorizer_macro_trailing_comma() {
 
 #[test]
 fn biscuit_macro() {
+    use biscuit_auth::PublicKey;
+    let pubkey = PublicKey::from_bytes(
+        &hex::decode("6e9e6d5a75cf0c0e87ec1256b4dfed0ca3ba452912d213fcc70f8516583db9db").unwrap(),
+    )
+    .unwrap();
     let b = biscuit!(
         r#"fact("test", hex:aabbcc, [ true], {my_key});
         rule($0, true) <- fact($0, $1, $2, {my_key});
-        check if {my_key}.starts_with("my");
+        check if {my_key}.starts_with("my") trusting {pubkey};
         "#,
         my_key = "my_value",
         other_key = false,
+        pubkey = pubkey,
     );
     assert_eq!(
         b.dump_code(),
         r#"fact("test", hex:aabbcc, [ true], "my_value");
 rule($0, true) <- fact($0, $1, $2, "my_value");
-check if "my_value".starts_with("my");
+check if "my_value".starts_with("my") trusting ed25519/6e9e6d5a75cf0c0e87ec1256b4dfed0ca3ba452912d213fcc70f8516583db9db;
 "#,
     );
 }
