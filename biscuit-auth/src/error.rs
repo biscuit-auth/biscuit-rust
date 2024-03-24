e58e7bda132f5fb83630ac6d4e992de53e71cb9c
baranyildirim@gmail.com <baran.yildirim@outlook.com>
2024-03-24 00:43:40 +0000

    Implement query_exactly_one

--- biscuit-auth/src/error.rs
+++ biscuit-auth/src/error.rs
@@ -292,6 +292,8 @@ pub enum RunLimit {
     TooManyIterations,
     #[error("spent too much time verifying")]
     Timeout,
+    #[error("Unexpected query results, expected {0} got {1}")]
+    UnexpectedQueryResult(usize, usize),
 }
 
 #[cfg(test)]
