95726afc80dbd0690d3ad5810443f7757d4deae6
Clement Delafargue <clement.delafargue@outscale.com>
2024-10-22 11:55:20 +0200

    bump datalog version when external calls are used

--- biscuit-auth/src/datalog/mod.rs
+++ biscuit-auth/src/datalog/mod.rs
@@ -990,7 +990,7 @@ fn contains_v3_3_op(expressions: &[Expression]) -> bool {
         expression.ops.iter().any(|op| match op {
             Op::Value(term) => contains_v3_3_term(term),
             Op::Closure(_, _) => true,
-            Op::Unary(Unary::TypeOf) => true,
+            Op::Unary(unary) => matches!(unary, Unary::TypeOf | Unary::Ffi(_)),
             Op::Binary(binary) => matches!(
                 binary,
                 Binary::HeterogeneousEqual
@@ -999,8 +999,8 @@ fn contains_v3_3_op(expressions: &[Expression]) -> bool {
                     | Binary::LazyOr
                     | Binary::All
                     | Binary::Any
+                    | Binary::Ffi(_)
             ),
-            _ => false,
         })
     })
 }
